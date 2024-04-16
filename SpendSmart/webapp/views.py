acec85f102e13c65435ebebf52a9d934f80bf92b
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-04-16 16:59:09 -0500

    Added home page

--- SpendSmart/webapp/views.py
+++ SpendSmart/webapp/views.py
@@ -10,11 +10,19 @@ def home(request):
     cur = connections['default'].cursor()
     username = request.POST.get("username")
     password = request.POST.get("password")
-    cur.execute("SELECT * from Credentials WHERE email = \'{}\'".format(username))
-    u,p = cur.fetchall()[0]
+    p =None
+    if cur.execute("SELECT * from Credentials WHERE email = \'{}\'".format(username)):
+        u,p = cur.fetchall()[0]
     if p!=password:
         return render(request,"index.html")
-    
-    
+    cur.execute("SELECT userid FROM User WHERE email = \'{}\'".format(username))
+    id = cur.fetchall()[0][0]
+    print(id)
+    cur.execute("SELECT txnId,title,timestamp,amount,note,paymentMethod,type,categoryId FROM Transaction WHERE userId = {}".format(id))
+    data = cur.fetchall()
+    # print(len(data),len(data[0]))
+    # data = []
+    context = {'txns':data}
     cur.close()
-    return HttpResponse("Success!")
+
+    return render(request,"home.html",context)
